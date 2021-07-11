d6ea6e30757dc7c9f50206a0ebd8c39b0debd19d
sofmega <soulf83@gmail.com>
2021-07-11 13:13:38 +0100

    First commit

---
+++ src/test/java/org/sid/cinema/Cinema1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cinema1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
